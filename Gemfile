19720914df8fd213595ed2daa3b595c214afdd7e
kyono <matsudayu03@gmail.com>
2019-06-13 03:13:03 +0000

    Add post annotate

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'rails-i18n'
+gem 'annotate'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  title      :string
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
     
     # Commentモデルを複数持てる様にする
