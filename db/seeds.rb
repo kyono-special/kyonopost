9e67bda252a014e1623dda78e58b309a30a2a647
kyono <matsudayu03@gmail.com>
2019-06-18 03:12:48 +0000

    Seeds production

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,18 @@ if Rails.env == 'development'
         { name: 'TDD' }
     ])
 end
+
+if Rails.env == 'production'
+    (1..100).each do |i|
+        Post.create(name: "ユーザ#{i}", title: "タイトル#{i}", content: "本文#{i}")
+    end
+    
+    # 追加
+    Tag.create([
+        { name: 'Ruby' },
+        { name: 'Ruby on Rails5' },
+        { name: 'Python3' },
+        { name: 'Django' },
+        { name: 'TDD' }
+    ])
+end
