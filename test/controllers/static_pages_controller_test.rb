bde12715eb6d7bd30b2792d3703a1dcbff473e32
kyono <matsudayu03@gmail.com>
2019-06-20 07:42:00 +0000

    Complete advanced setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,14 +2,5 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "POSTN"
-  end
-  
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
   
 end
